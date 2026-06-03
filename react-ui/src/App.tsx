9f142d613d8a05583a27a1f25064931405e0bf58
Amos Wagon <amos.wagon@aspentech.com>
2026-06-03 10:08:40 -0400

    refactor: remove user form and related functionality from App component

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <form className="user-form" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input id="name" name="name" type="text" required />
-
-          <label htmlFor="dob">Date of Birth</label>
-          <input id="dob" name="dob" type="date" required />
-
-          <button type="submit">Submit</button>
-        </form>
-      </header>
-    </div>
-  );
+  return null;
 }
 
 export default App;
